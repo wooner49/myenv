51eeda66cda22f2d21530734ef68a6e6b45a2e8a
Dongjin Lee <wooner49@gmail.com>
2022-12-14 07:23:56 +0000

    add dgl

--- init_container.sh
+++ init_container.sh
@@ -15,4 +15,5 @@ apt-get install -y libxrender1 g++ tmux
 pip install --upgrade pip
 pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.1+cu113.html
 pip install scipy pandas
+pip install dgl-cu113 dglgo -f https://data.dgl.ai/wheels/repo.html
 
